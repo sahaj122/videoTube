a3cabaecfd2b5267634e98b1f1faf751924bcee7
SAHAJJIT SINGH <sahajjitsingh@gmail.com>
2024-06-14 23:26:44 +0530

    adding multer middleware, setting up routes andcontrollers, all set-up done, good to take actual data from user

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-const app = express()
+const app = express()   
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,5 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+import userRoute from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users", userRoute)
 
 export { app }
